23755e65597bcef93708a0fa0e8d1e5cf5679b18
youcefking <32298602+youcefking@users.noreply.github.com>
2022-08-15 16:39:44 +0100

    sit no get up

--- teamp/Assets/codes/PlayerController.cs
+++ teamp/Assets/codes/PlayerController.cs
@@ -10,13 +10,24 @@ public class PlayerController : MonoBehaviour
     int jumpavailable = 1;
     float moveVelocity;
     public int jumpsnumber = 1;
-    //Grounded Vars
+    public Transform player;
     
 
     void start(){jumpavailable = jumpsnumber;}
 
     void Update () 
     {
+        //sitting
+        if (Input.GetKeyDown (KeyCode.DownArrow) || Input.GetKeyDown (KeyCode.S)) 
+        {
+             Vector3 plp = player.position;
+             plp.y -= 0.35f;
+             player.position = plp;
+
+             Vector3 pls = player.localScale;
+             pls.y -= 0.35f;
+             player.localScale = pls;
+        }
         //Jumping
         if (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.Z) || Input.GetKeyDown (KeyCode.W)) 
         {
